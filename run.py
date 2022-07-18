2c46957624cc60d4af84be50b0a92b65e5dba972
DanTaylor28 <dan.taylor1493@gmail.com>
2022-07-18 10:10:08 +0000

    intro function to collect users name and handle any input errors

--- run.py
+++ run.py
@@ -1,3 +1,28 @@
 import random
 
+def intro():
+    """
+    Prints a welcome message to the terminal and an input to get the users name.
+    Entering non alphabetic symbols or an input less than 2 letters raises an error
+    which is handled by a try/except statement.
+    """
+    print('Welcome to Hangman! Guess the correct word before you are hung!')
+    print('Guess 1 letter at a time or the full word if you feel confident.')
+
+    while True:
+        try:
+            name = (str(input('Please enter your name below:\n')))
+            if name.isalpha() and len(name) > 1:
+                print(f'Hello {name}, we hope you enjoy playing & best of luck!')
+                break
+            else:
+                raise TypeError
+        except TypeError as e:
+                print('The name you entered is invalid.. Please try again.')
+                continue
+
+
+intro()
+
+
 
