bccfcdded82830366f1432c55513a19103c8e9f2
DanTaylor28 <dan.taylor1493@gmail.com>
2022-07-19 11:40:13 +0000

    Print out hangman animation & incorrectly guessed letters

--- run.py
+++ run.py
@@ -61,17 +61,26 @@ def intro():
 
 def get_random_word(random_word):
     """
-    Returns a random word from the list of game words defined above.
+    Returns a random word from the list of game words defined above 
+    using the random pack imported at the top of the code.
     """
     word = random.randint(0, len(random_word) -1)
     print(random_word[word])
 
-
-
+def game_board(game_word, correct_guesses, incorrect_guesses):
+    """
+    Print out the expected animation of hangman to correspond to how many
+    incorrect guesses have been made.
+    Also prints out an f string showing the incorrect guesses made by 
+    the user.
+    """
+    print(HANGMAN[len(incorrect_guesses)])
+    print(f'Incorrectly guessed letters: {incorrect_guesses}')
+    
 
 intro()
 get_random_word(GAME_WORDS)
-print(HANGMAN[0])
+
 
 
 
